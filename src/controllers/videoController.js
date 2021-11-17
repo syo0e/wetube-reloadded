a0f6bf41d8895e33b2883f89c10491bccd8982c5
kimdlzp <kimdlzp@gmail.com>
2021-11-17 12:18:00 +0900

    first commit

---
+++ src/controllers/videoController.js
@@ -0,0 +1,8 @@
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("Upload");
+export const deleteVideo = (req, res) => {
+  return res.send("Delete Video");
+};
