70e25027b2aa12580439f39fa7d7ba9d35f8ce60
kimdlzp <kimdlzp@gmail.com>
2022-02-14 20:47:02 +0900

    More practice 1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -44,3 +44,10 @@ export const postEdit = (req, res) => {
   videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = (req, res) => {
+  return res.redirect("/");
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
 import {
   watch,
-  upload,
-  deleteVideo,
+  getUpload,
   getEdit,
   postEdit,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -13,5 +13,6 @@ videoRouter.get("/:id(\\d+)", watch);
 // videoRouter.get("/:id(\\d+)/edit", getEdit);
 // videoRouter.post("/:id(\\d+)/edit", postEdit);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
